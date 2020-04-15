1e39bde4aaf521601d04863546c503b144e63281
zims <zims@inbox.lv>
2020-04-15 17:04:38 +0300

    output folder

--- combine.py
+++ combine.py
@@ -2,11 +2,16 @@
 import PyPDF2
 import sys
 import glob
-
+import os
 
 # it combines the pdfs in list
 # create it combines all that is in a folder
 inputs = []
+
+try:
+    os.mkdir('output')
+except OSError:
+    pass
 
 
 def pdf_combiner(pdf_list):
@@ -14,8 +19,7 @@ def pdf_combiner(pdf_list):
 
     for pdf in pdf_list:
         merger.append(pdf)
-    merger.write('super.pdf')
-
+    merger.write('output/super.pdf')
 
 for filename in glob.glob(f'combine_me/*.pdf'):  # assuming pdf
     inputs.append(filename)
